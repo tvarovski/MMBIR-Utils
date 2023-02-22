683cce5eb316b09f2bffc61c6c1b09828cd2648e
tvarovski <tvarovski1@gmail.com>
2023-02-22 13:31:43 -0600

    add log redirect to run_all_filters.sh

--- tools_package/run_all_filters.sh
+++ tools_package/run_all_filters.sh
@@ -1,5 +1,9 @@
-./filterDirs2.sh
-./filterDirs2.sh -f
+#!/bin/bash
 
+# first run program without -f (filtering) option, redirect output to log file and error file
+./filterDirs2.sh 1> outputs/filterDirs2.log 2> outputs/filterDirs2.err &
+
+# then run program with -f (filtering) option, redirect output to log file and error file
+./filterDirs2.sh -f 1> outputs/filterDirs2_f.log 2> outputs/filterDirs2_f.err &
 
 echo "Done!"
