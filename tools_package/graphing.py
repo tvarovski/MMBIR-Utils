a81408dfc590b38df8601f473a043da724e9d93c
tvarovski <tvarovski1@gmail.com>
2023-03-06 19:27:38 -0600

    add error handling to graphing

--- tools_package/graphing.py
+++ tools_package/graphing.py
@@ -4,6 +4,7 @@
 from tools import *
 import cancer_config as cfg
 import os
+import logging
 
 logger = logging.getLogger(__name__)
 
@@ -28,39 +29,82 @@ def graphing(params, save=False, show=True):
 
     df_consolidated = annotate_consolidated_results(df_consolidated, df_metadata)
 
-    plot_differential_expression(cancer, save=save, show=show)
+    try:
+        plot_differential_expression(cancer, save=save, show=show)
+    except Exception as e:
+        logging.error(f"Error in differential expression plot: {e}")
 
-    plot_total_reads_vs_count(df_consolidated, count="Raw_Count", min_concentration=min_concentration, save=save, show=show)
+    try:
+        plot_total_reads_vs_count(df_consolidated, count="Raw_Count", min_concentration=min_concentration, save=save, show=show)
+    except Exception as e:
+        logging.error(f"Error in total reads vs count plot: {e}")
 
-    df_consolidated = df_consolidated[df_consolidated["total_reads"] >= 100000000]
-    print("removing samples with fewer than 100000000 reads")
+    try:
+        df_consolidated_trimmed = df_consolidated[df_consolidated["total_reads"] >= 100000000].copy()
+        print("removing samples with fewer than 100000000 reads")
+        plot_total_reads_vs_count(df_consolidated_trimmed, count="Raw_Count", min_concentration=min_concentration, save=save, show=show)
+    except Exception as e:
+        logging.error(f"Error in total reads vs count plot: {e}")
 
-    plot_total_reads_vs_count(df_consolidated, count="Raw_Count", min_concentration=min_concentration, save=save, show=show)
+    try:
+        df_wide = plot_blood_tumor_count_correlations_treshold_delta(df_consolidated, min_concentration=min_concentration, method="spearman", save=save, show=show) #method="spearman" or "pearson"
+        plot_blood_tumor_count_correlation(df_wide, method="spearman", threshold=3000, save=save, show=show) #method="spearman" or "pearson"
+    except Exception as e:
+        logging.error(f"Error in blood tumor count correlation plot: {e}")
 
-    df_wide = plot_blood_tumor_count_correlations_treshold_delta(df_consolidated, min_concentration=min_concentration, method="spearman", save=save, show=show) #method="spearman" or "pearson"
-    plot_blood_tumor_count_correlation(df_wide, method="spearman", threshold=3000, save=save, show=show) #method="spearman" or "pearson"
-
-    plot_count_vs_concentration(df_consolidated, x_count="Raw_Count", save=save, show=show)
+    try:
+        plot_count_vs_concentration(df_consolidated, x_count="Raw_Count", save=save, show=show)
+    except Exception as e:
+        logging.error(f"Error in count vs concentration plot: {e}")
 
     filterset=["Blood Derived Normal","Primary Tumor"] #"Blood Derived Normal", "Primary Tumor"
 
-    plot_concentration_raw_filtered(df_consolidated, filterset, hue="Concentration", save=save, show=show) #Concentration_bin
-    plot_Sample_Type_counts(df_consolidated, filterset, min_concentration=min_concentration, cancer=cancer, save=save, show=show)
+    try:
+        plot_concentration_raw_filtered(df_consolidated, filterset, hue="Concentration", save=save, show=show) #Concentration_bin
+    except Exception as e:
+        logging.error(f"Error in concentration raw filtered plot: {e}")
 
-    filterset=["Primary Tumor"] #"Blood Derived Normal", "Primary Tumor"
+    try:
+        plot_Sample_Type_counts(df_consolidated, filterset, min_concentration=min_concentration, cancer=cancer, save=save, show=show)
+    except Exception as e:
+        logging.error(f"Error in sample type counts plot: {e}")
+
+    filterset=["Primary Tumor"] #"Blood Derived Normal", "Primary Tumor", control_tumor
 
     #staging='ajcc'/'figo'/'tumor_grade', adjust='early_late' or 'early_middle_late'
-    plot_stage_vs_count(df_consolidated, filterset, staging=staging, x_count="Filtered_Count", min_concentration=min_concentration, adjust_staging='early_late', save=save, show=show)
-    plot_stage_vs_concentration(df_consolidated, filterset, staging=staging, x_count="Filtered_Count", min_concentration=min_concentration, adjust_staging='early_late', save=save, show=show)
+    try:
+        plot_stage_vs_count(df_consolidated, filterset, staging=staging, x_count="Filtered_Count", min_concentration=min_concentration, adjust_staging='early_late', save=save, show=show)
+    except Exception as e:
+        logging.error(f"Error in stage vs count plot: {e}")
+    try:
+        plot_stage_vs_concentration(df_consolidated, filterset, staging=staging, x_count="Filtered_Count", min_concentration=min_concentration, adjust_staging='early_late', save=save, show=show)
+    except Exception as e:
+        logging.error(f"Error in stage vs concentration plot: {e}")
 
     if staging == 'ajcc':
-        plot_ajcc_pathologic_n_vs_count(df_consolidated, filterset, x_count="Filtered_Count", min_concentration=min_concentration, save=save, show=show)
-        plot_ajcc_pathologic_t_vs_count(df_consolidated, filterset, x_count="Filtered_Count", min_concentration=min_concentration, save=save, show=show)
-
-    plot_age_vs_count_correlation(df_consolidated, count="Filtered_Count", min_concentration=min_concentration, method="spearman", save=save, show=show)
+        try:
+            plot_ajcc_pathologic_n_vs_count(df_consolidated, filterset, x_count="Filtered_Count", min_concentration=min_concentration, save=save, show=show)
+        except Exception as e:
+            logging.error(f"Error in ajcc pathologic n vs count plot: {e}")
+        try:
+            plot_ajcc_pathologic_t_vs_count(df_consolidated, filterset, x_count="Filtered_Count", min_concentration=min_concentration, save=save, show=show)
+        except Exception as e:
+            logging.error(f"Error in ajcc pathologic t vs count plot: {e}")
     
-    plot_age_vs_count_binned(df_consolidated, count="Filtered_Count", min_concentration=min_concentration, save=save, show=show)
-    plot_age_vs_count_binned(df_consolidated, count="Raw_Count", min_concentration=min_concentration, save=save, show=show)
+    try:
+        plot_age_vs_count_correlation(df_consolidated, count="Filtered_Count", min_concentration=min_concentration, method="spearman", save=save, show=show)
+    except Exception as e:
+        logging.error(f"Error in age vs count correlation plot: {e}")
+
+    try:
+        plot_age_vs_count_binned(df_consolidated, count="Filtered_Count", min_concentration=min_concentration, save=save, show=show)
+    except Exception as e:
+        logging.error(f"Error in age vs count binned plot: {e}")
+    
+    try:
+        plot_age_vs_count_binned(df_consolidated, count="Raw_Count", min_concentration=min_concentration, save=save, show=show)
+    except Exception as e:
+        logging.error(f"Error in age vs count binned plot: {e}")
 
 if __name__ == "__main__":
 
