f70a31a6b5ce84729f81caff7afa2e5aa2676508
tvarovski <tvarovski1@gmail.com>
2023-04-04 20:00:04 -0500

    fix

--- tools_package/tools/graphing_base.py
+++ tools_package/tools/graphing_base.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 ''' This code was developed and authored by Jerzy Twarowski in Malkova Lab at the University of Iowa 
     Contact: jerzymateusz-twarowski@uiowa.edu, tvarovski1@gmail.com
 '''
--- tools_package/utils.py
+++ tools_package/utils.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 # This code was developed and authored by Jerzy Twarowski in Malkova Lab at the University of Iowa 
 # Contact: jerzymateusz-twarowski@uiowa.edu, tvarovski1@gmail.com
 
